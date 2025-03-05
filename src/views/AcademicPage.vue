356f8d3005be12aa10b1fda792de71b0b1989736
Phyo_Min_Thein <6531503172@lamduan.mfu.ac.th>
2025-03-06 00:11:24 +0700

    Update Dean Page

--- src/views/AcademicPage.vue
+++ src/views/AcademicPage.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="powerbi-container">
-    <iframe
-      title="Academic_Service"
-      width="100%"
-      height="500"
-      src="https://your-powerbi-academic-url.com"
-      frameborder="0"
-      allowFullScreen="true"
-    ></iframe>
-  </div>
-</template>
+    <div class="powerbi-container">
+      <iframe
+        title="Publication_test"
+        width="100%"
+        height="800"
+        src="https://app.powerbi.com/view?r=eyJrIjoiOTdhZjQ1MWEtYmQzNi00MDYyLWJlOGItNzc0YTUwYzljMmY3IiwidCI6ImU5NGQ1N2FkLWEwYjctNDRjZi04ODNmLWQ3OTg3YTBiZDI0NCIsImMiOjEwfQ%3D%3D"
+        frameborder="0"
+        allowFullScreen="true"
+      ></iframe>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "AcademicPage",
+  };
+  </script>
+  
+  <style scoped>
+  .powerbi-container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  iframe {
+    width: 100%;
+    height: 800px;
+  }
+  </style>
 
-<script>
-export default {
-  name: "AcademicPage",
-};
-</script>
--- src/views/MouPage.vue
+++ src/views/MouPage.vue
@@ -1,19 +1,33 @@
-// MouPage.vue (Placeholder Power BI link)
 <template>
-  <div class="powerbi-container">
-    <iframe
-      title="MOU_MOA"
-      width="100%"
-      height="500"
-      src="https://your-powerbi-mou-url.com"
-      frameborder="0"
-      allowFullScreen="true"
-    ></iframe>
-  </div>
-</template>
-
-<script>
-export default {
-  name: "MouPage",
-};
-</script>
+    <div class="powerbi-container">
+      <iframe
+        title="Publication_test"
+        width="100%"
+        height="800"
+        src="https://app.powerbi.com/view?r=eyJrIjoiMDI4NmUwNDItM2IzZi00ODA1LTg0MGQtOWI2YmIyZDllYzdjIiwidCI6ImU5NGQ1N2FkLWEwYjctNDRjZi04ODNmLWQ3OTg3YTBiZDI0NCIsImMiOjEwfQ%3D%3D"
+        frameborder="0"
+        allowFullScreen="true"
+      ></iframe>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "MouPage",
+  };
+  </script>
+  
+  <style scoped>
+  .powerbi-container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  iframe {
+    width: 100%;
+    height: 800px;
+  }
+  </style>
