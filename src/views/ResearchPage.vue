0cc5833b8af5a3e43a872d6323e5c464236a57bb
Phyo_Min_Thein <6531503172@lamduan.mfu.ac.th>
2025-03-05 22:02:13 +0700

    Change Size

--- src/views/ResearchPage.vue
+++ src/views/ResearchPage.vue
@@ -1,29 +1,33 @@
-// ResearchPage.vue
 <template>
-  <div class="powerbi-container">
-    <iframe
-      title="Publication_test"
-      width="100%"
-      height="500"
-      src="https://app.powerbi.com/view?r=eyJrIjoiNTAyY2RiYzUtZGFhOC00YmIxLTlmNDYtMzhjZmE1YjUxMzRhIiwidCI6ImU5NGQ1N2FkLWEwYjctNDRjZi04ODNmLWQ3OTg3YTBiZDI0NCIsImMiOjEwfQ%3D%3D"
-      frameborder="0"
-      allowFullScreen="true"
-    ></iframe>
-  </div>
-</template>
-
-<script>
-export default {
-  name: "ResearchPage",
-};
-</script>
-
-<style scoped>
-.powerbi-container {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-</style>
+    <div class="powerbi-container">
+      <iframe
+        title="Publication_test"
+        width="100%"
+        height="800"
+        src="https://app.powerbi.com/view?r=eyJrIjoiNTAyY2RiYzUtZGFhOC00YmIxLTlmNDYtMzhjZmE1YjUxMzRhIiwidCI6ImU5NGQ1N2FkLWEwYjctNDRjZi04ODNmLWQ3OTg3YTBiZDI0NCIsImMiOjEwfQ%3D%3D"
+        frameborder="0"
+        allowFullScreen="true"
+      ></iframe>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "ResearchPage",
+  };
+  </script>
+  
+  <style scoped>
+  .powerbi-container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  iframe {
+    width: 100%;
+    height: 800px;
+  }
+  </style>
